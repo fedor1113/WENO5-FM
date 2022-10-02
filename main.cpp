50059ab487f513ad8c81514dfd6fcff7dc09d83a
fedor1113 <fedor185@hotmail.com>
2022-10-02 19:58:39 +0300

    Change `main.cpp`

--- main.cpp
+++ main.cpp
@@ -4,24 +4,29 @@
 
 #include <algorithm>
 #include <cstdio>
+#include <cstdlib>
+// #include <format>
 #include <fstream>
 // #include <initializer_list>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <numeric>
+#include <string>
 #include <valarray>
 #include <vector>
 
 // #include "vectorfieldcomponentview.h"
-// #include "euler1d.h"
+#include "euler1d.h"
 // #include "kfr1d.h"
-#include "inviscidburgers1d.h"
+// #include "inviscidburgers1d.h"
 
-// template class Vector4<numeric_val>;
-// using Vec4 = Vector4<numeric_val>;
+template class Vector4<numeric_val>;
+using Vec4 = Vector4<numeric_val>;
 
 
 int main(int argc, char **argv) {
-	std::ios::sync_with_stdio(false);
+	// std::ios::sync_with_stdio(false);
 	// std::cin.tie(nullptr);
 
 	std::size_t k = 0;
@@ -36,28 +41,9 @@ int main(int argc, char **argv) {
 	numeric_val t = 0.;
 	numeric_val tfinal = 0.0;  // [T]
 
-	// std::valarray<Vec4> u_init(Vec4::ZERO, N_full);
-	// std::valarray<Vec4> flux(Vec4::ZERO, N_full);
-	// std::valarray<Vec4> Y2(Vec4::ZERO, N_full);
-	// std::valarray<Vec4> Y3(Vec4::ZERO, N_full);
-
-	// numeric_val gamma = 1.4;
-
-//	std::valarray<numeric_val> s1(static_cast<numeric_val>(0.), N_full);
-//	for (k = 0; k < N_full; ++ k)
-//		s1[k] = u_init[k][3] / u_init[k][0];  // s2 = W[4,:]/W[0,:]
-//	std::valarray<numeric_val> s2 = 1 - s1;
-
-	// std::size_t Nt = std::ceil(tfinal / dt);
-	// std::valarray<int> Ts = std::valarray(0, Nt+2);
-	// for (std::size_t k = 0; k <= Nt+1; ++ k) Ts[k] = k;
-
-	// for (auto n : Ts) {
+	numeric_val gamma = 1.4;
 
 	// std::valarray<Vec4> u_res(Vec4::ZERO, N_full);
-//	for (auto n : VectorFieldComponentView<std::valarray<Vec4>>(u_res, 3))
-//		std::cout << n << "\n";
-
 
 	// tfinal = 0.2;
 	// solve1DRiemannProblemForEulerEq<numeric_val>(
@@ -201,23 +187,40 @@ int main(int argc, char **argv) {
 	for (std::size_t j
 //		 : {51, 101, 151, 201, 251, 501, 751, 801,
 //			1001, 1101, 1251, 1501,
+//			2001, 2501, 5001,
 //			10001}) {
-		 : {101}) {
+//		 : {21, 41, 81, 161, 321, 641}) {
+		 : {200}) {
+//		 : {1001}) {
+//		 : {26, 51, 101, 201, 401, 801/*, 1601, 3206*/}) {
+//		 : {50, 100, 200, 400, 800, 1600/*, 3200, 6400*/}) {
 		Nx = j; N_full = Nx + 2 * N_ghost_points;
 		L = 10.;
 		// L = 1.;
-		cfl = 0.8;
+		cfl = 1.;
 		tfinal = 3000.;
 		// tfinal = 1.5;
 
 
-		std::valarray<numeric_val> u_res(0., N_full);
+		// std::valarray<numeric_val> u_res(0., N_full);
+		std::valarray<Vec4> u_res(Vec4::ZERO, N_full);
 //		std::size_t guessed_length = static_cast<std::size_t>(
 //					(tfinal - t) / (cfl * L / Nx / 10)
 //					);
 //		std::vector<numeric_val> u_s(guessed_length, 0.);
 //		std::vector<numeric_val> times(u_s.size(), 0.);
 		std::valarray<numeric_val> x(0., N_full);
+
+		L = 1.;
+		cfl = 0.9;
+		tfinal = 0.2;
+		solve1DRiemannProblemForEulerEq<numeric_val>(
+			u_res, x, gamma,
+			1., 0.75, 1.,
+			0.125, 0., 0.1, 0.3,
+			t, tfinal, 0., L,
+			primitiveToConservativeU<numeric_val>, Nx, cfl
+		);  // Modified Sod's problem (expansion-contact-shock). Toro-1
 
 //		solve1DDetonationProfileProblem<numeric_val>(
 //					u_res, x,
@@ -235,32 +238,39 @@ int main(int argc, char **argv) {
 //			0., 0.5/std::numbers::pi_v<numeric_val>, -1., 1.,
 //			Nx, cfl
 //		);
-		tfinal = 1.;
-		L = 1.;
-		solve1DInviscidBurgersProblem<numeric_val>(
-			u_res, x,
-			0., tfinal, -L, L,
-			Nx, cfl, 1
-		);  // Toro-1
+//		tfinal = 2.;
+//		L = 1.;
+//		solve1DInviscidBurgersProblem<numeric_val>(
+//			u_res, x,
+//			0., tfinal, -1., 1.,
+//			Nx, cfl, 2
+//		);  // Henrick-1
 
 		std::ofstream outfile;
 
-		outfile.open("res_n_" + std::to_string(j) + ".dat");
+		std::string folder = "./";
+
+		std::string filepath = folder
+				+ "res_n_"
+				+ std::to_string(j)
+				+ ".dat";
+
+		outfile.open(filepath);
 
 		k = 0;
 		if (outfile.is_open()) {
-			// outfile << "TITLE=\"Riemann Problem 1D slice t="
-			// 		<< tfinal << "\"" << "\n";
-			// outfile << "VARIABLES=\"x\",\"rho\",\"u\",\"p\",\"e\"" << "\n";
-			// outfile << "VARIABLES=\"x\",\"rho\",\"u\",\"p\"" << "\n";
-			outfile << "TITLE=\"Detonation Problem 1D slice t="
+			 outfile << "TITLE=\"Riemann Problem 1D slice t="
 					<< tfinal << "\"" << "\n";
-			outfile << "VARIABLES=\"x\",\"u\"" << "\n";
-			outfile << "ZONE T=\"Numerical\", I="
-					<< N_full << ", F=POINT" << "\n";
+			 outfile << "VARIABLES=\"x\",\"rho\",\"u\",\"p\",\"e\"" << "\n";
+			 outfile << "VARIABLES=\"x\",\"rho\",\"u\",\"p\"" << "\n";
+//			outfile << "TITLE=\"Detonation Problem 1D slice t="
+//					<< tfinal << "\"" << "\n";
+//			outfile << "VARIABLES=\"x\",\"u\"" << "\n";
+//			outfile << "ZONE T=\"Numerical\", I="
+//					<< N_full << ", F=POINT" << "\n";
 
 			for (auto u : u_res) {
-				// Vec4 q = conservativeToPrimitive(u, gamma);
+				Vec4 q = conservativeToPrimitive(u, gamma);
 				// std::cout << u << "\n";
 
 				//			std::cout << x[k]
@@ -274,12 +284,20 @@ int main(int argc, char **argv) {
 				//					<< " " << u[1]/u[0]
 				//					<< " " << (u[2]-u[1]*(u[1]/u[0])/2)*(G-1)
 				//					<< " " << u[3]/u[0] << "\n";
-				// outfile << x[k]
-				// 		<< " " << q[0]
-				// 		<< " " << q[1]
-				// 		<< " " << q[2]
-				// 		<< " " << q[3] << "\n";
-				outfile << x[k] << " " << u << "\n";
+				outfile << std::setprecision(
+							   std::numeric_limits<numeric_val>::max_digits10 - 1)
+						<< std::scientific
+						<< x[k]
+						<< " " << q[0]
+						<< " " << q[1]
+						<< " " << q[2]
+						<< " " << q[3] << "\n";
+//				outfile << std::format("{}", x[k])
+//						<< " " << std::format("{}", u) << "\n";
+//				outfile << std::setprecision(
+//							   std::numeric_limits<numeric_val>::max_digits10 - 1)
+//						<< std::scientific
+//						<< x[k] << " " << u << "\n";
 
 				++ k;
 			}
@@ -287,8 +305,12 @@ int main(int argc, char **argv) {
 
 		outfile.close();
 
+//		std::string u_filepath = folder
+//				+ "u_res_n_"
+//				+ std::to_string(j)
+//				+ ".dat";
 
-//		outfile.open("u_res_n_" + std::to_string(j) + ".dat");
+//		outfile.open(u_filepath);
 
 //		k = 0;
 //		if (outfile.is_open()) {
@@ -303,14 +325,24 @@ int main(int argc, char **argv) {
 //					<< times.size() << ", F=POINT" << "\n";
 
 //			for (std::size_t k = 0; k < times.size(); ++ k) {
-//				outfile << times[k] << " " << u_s[k] << "\n";
+//				outfile << std::setprecision(
+//							   std::numeric_limits<numeric_val>::max_digits10 - 1)
+//						<< std::scientific
+//						<< times[k] << " " << u_s[k] << "\n";
 //			}
 //		}
 
 //		outfile.close();
 
 
-		std::cout << "Done!" << "\n";
+		std::cout << j << " Done!" << "\n";
+
+//		std::system((
+//			"gnuplot -e \"filename='" + filepath + "'\" plot.gnuplot"
+//		).c_str());
+//		std::system((
+//			"gnuplot -e \"filename='" + u_filepath + "'\" plot.gnuplot"
+//		).c_str());
 	}
 	return 0;
 }
--- plot.gnuplot
+++ plot.gnuplot
@@ -1,13 +1,19 @@
 #!/usr/bin/env -S gnuplot -persist
-# set style data linespoints
-set style data lines
+set style data linespoints
+# set style data lines
 set key outside top center horizontal
 set offsets graph 0.1, graph 0.1, graph 0.1, graph 0.1
 set title "Instantaneous Profile"
 set grid xtics
 set grid ytics
 set grid
-plot "< awk '(NR>2){print;}' res.dat" using 1:2 title "u"
+
+process = "< awk '(NR>2){print;}' "
+file = filename
+plt_data = process.file
+plot plt_data using 1:2 title "u_h", \
+    # sin(pi * (x - 2.) - sin(pi * (x - 2.)) / pi) title "u_a"
+	# exp(-8. * (x-1.) * (x-1.)) title "u_a"
 	# "< awk '(NR>2){print;}' res.dat" using 1:4 title "p", \
 	# "< awk '(NR>2){print;}' res.dat" using 1:2 title "ρ", \
 	# "< awk '(NR>2){print;}' res.dat" using 1:3 title "u", \
